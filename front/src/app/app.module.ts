fc22cbd03a7728c484f37150da98a0820b906845
Franco Osores <franco_osores@hotmail.com>
2022-07-05 15:46:33 -0300

    Feat: Layout and clientSide validations completed for Login Form Component

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material/material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './features/components/login-form/login-form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front/src/app/features/components/login-form/login-form.component.ts
+++ front/src/app/features/components/login-form/login-form.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss']
+  styleUrls: ['./login-form.component.scss'],
 })
 export class LoginFormComponent implements OnInit {
-
-  constructor() { }
+  userLogin: FormGroup = new FormGroup({});
+  
+  constructor() {}
 
   ngOnInit(): void {
+    this.userLogin = new FormGroup({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z]+@certant\.com$/),
+      ]),
+      password: new FormControl('', Validators.required),
+    });
   }
-
 }
