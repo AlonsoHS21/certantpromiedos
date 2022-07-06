9e7b398733ab101360b55f891936cf48d975afe4
Alonso <61801479+AlonsoHS21@users.noreply.github.com>
2022-07-06 10:51:03 -0300

    Metodo de Agregar usuario en UsuarioController

--- Qatar2022/Qatar2022/src/main/java/com/qatar/proyecto/controllers/UsuarioController.java
+++ Qatar2022/Qatar2022/src/main/java/com/qatar/proyecto/controllers/UsuarioController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,16 +23,26 @@ public class UsuarioController {
 	@Qualifier("usuarioService") //Se usa para diferenciar las implementaciones
 	private UsuarioService usuarioService;
 	
+	
 	//@GetMapping es un atajo para @RequestMapping(method = RequestMethod.GET) y se utiliza para mapear HTTP GET solicitudes a los métodos de controlador asignados. 
 	@GetMapping("/listaUsuarios") 
 	public List<Usuario> listar() {
 		return usuarioService.listarUsuarios();
 	}
 	
+	@PostMapping 
+	public Usuario agregar(@RequestBody Usuario usuario) {
+		return usuarioService.guardar(usuario);
+	}
+	
+	
+	
 /* Links Utiles para las anotaciones
  *  https://ricardogeek.com/spring-boot-y-la-anotacion-crossorigin/
  *  https://pharos.sh/controller-y-restcontroller-anotaciones-en-spring-boot/
  *  https://www.arquitecturajava.com/spring-qualifier-utilizando-autowired/
+ *  https://mvitinnovaciontecnologica.wordpress.com/2020/02/06/guia-de-anotaciones-de-spring-framework/
+ *  https://gustavopeiretti.com/estructura-de-paquetes-spring-boot/
  *  Nota: @ResponseBody Cuando los controladores de solicitudes devuelven datos, como return repository.findById(), la respuesta se serializará en JSON antes de devolverse al cliente.
  * */
 	
