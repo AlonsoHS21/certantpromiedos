199f404db7734e6bce9dbcf54ad1c187063d64f4
Alonso <61801479+AlonsoHS21@users.noreply.github.com>
2022-07-06 22:14:40 -0300

    EquipoController con los metodos agregar, modificar, eliminar y listar

--- Qatar2022/Qatar2022/src/main/java/com/qatar/proyecto/controllers/EquipoController.java
+++ Qatar2022/Qatar2022/src/main/java/com/qatar/proyecto/controllers/EquipoController.java
@@ -1,5 +1,7 @@
 package com.qatar.proyecto.controllers;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,21 +29,44 @@ public class EquipoController {
 	@Qualifier("equipoService")
 	private IEquipoService equipoService;
 	
-	@RequestMapping("/")
-	public String crear(Model model) {
-		Equipo equipo = new Equipo();
-		model.addAttribute("equipo", equipo);
-		return "equipo/crear";
+	@GetMapping("/")
+	public String lista(Model model) {
+		List<Equipo> listaEquipo = equipoService.getAll();
+		model.addAttribute("equipo",listaEquipo);
+		return "equipo/lista";
 	}
 	
+	@GetMapping("/crear")
+	public String crear(Equipo equipo) {
+		return "equipo/crear"; //Me envia a crear.html
+	}
+	
+	@PostMapping("/guardar")
+	public String guardar(Equipo equipo) {
+		equipoService.save(equipo);
+		return "redirect:/equipo/"; //Me envia a la lista de equipos
+	}
+	
+	@GetMapping("/editar/{idEquipo}")
+	public String editar(Equipo equipo, Model model) { //Inicializa con los datos del idEquipo
+		equipo = equipoService.findByNombre(equipo.getNombre());
+		model.addAttribute("persona",equipo);
+		return "equipo/crear"; //crear.html sirve para modificar y crear
+	}
+	
+	@GetMapping("/eliminar/{idEquipo}")
+	public String eliminar(Equipo equipo) {
+		equipoService.eliminar(equipo.getIdEquipo());
+		return "redirect:/equipo/";	//Me envia a la lista de equipos
+	}
+	
+	/*
 	@PostMapping("/")
 	public String guardar(@Valid @ModelAttribute Equipo equipo, BindingResult result, Model model, RedirectAttributes attribute) {
 		if(equipoService.findByNombre(equipo.getNombre())!= null) {
 			FieldError error = new FieldError("equipo", "nombre", "Ya existe un equipo con este nombre");
 			result.addError(error);
-			
 		}
-		
 		
 		if(result.hasErrors()) {
 			System.out.println("Hubo error en el formulario!");
@@ -50,6 +76,6 @@ public class EquipoController {
 		System.out.println("Equipo guardado con exito!");
 		attribute.addFlashAttribute("success","Equipo creado con exito");
 		return "redirect:/equipo/";
-	}
+	}*/
 
 }
