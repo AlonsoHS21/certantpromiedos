f7e63b1aa307defc1e9e56f9dcfb5fceb12bdbab
Alonso <61801479+AlonsoHS21@users.noreply.github.com>
2022-07-06 21:18:46 -0300

    Cambio en la clase Usuario
    Solo tiene como atributos email y contrasenia ya que no se crean usuarios nuevos

--- Qatar2022/Qatar2022/src/main/java/com/qatar/proyecto/entities/Usuario.java
+++ Qatar2022/Qatar2022/src/main/java/com/qatar/proyecto/entities/Usuario.java
@@ -18,21 +18,11 @@ public class Usuario {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(name="nombre")
-	private String nombre;
-	
-	@Column(name="apellido")
-	private String apellido;
-	
 	@Column(name="contrasenia")
 	private String contrasenia;
 	
 	@Column(name="email")
 	private String email;
-	
-	@NotNull
-	@Column(name = "puntos")
-	private Long puntos;
 
 	public Usuario() {
 		
@@ -44,22 +34,6 @@ public class Usuario {
 	
 	public void setId(Long id) {
 		this.id = id;
-	}
-	
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getApellido() {
-		return apellido;
-	}
-
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
 	}
 
 	public String getContrasenia() {
@@ -77,13 +51,4 @@ public class Usuario {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
-	public Long getPuntos() {
-		return puntos;
-	}
-
-	public void setPuntos(Long puntos) {
-		this.puntos = puntos;
-	}
-	
 }
