14ca5251f28cdb61fbdbc49e45c713a2ce7ce470
Alonso <61801479+AlonsoHS21@users.noreply.github.com>
2022-07-08 13:14:44 -0300

    Agrego Enpoints para buscar por id y eliminar Jugadores

--- Qatar2022/Qatar2022/src/main/java/com/qatar/proyecto/controllers/JugadorController.java
+++ Qatar2022/Qatar2022/src/main/java/com/qatar/proyecto/controllers/JugadorController.java
@@ -5,11 +5,15 @@ import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +22,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.qatar.proyecto.entities.Equipo;
 import com.qatar.proyecto.entities.Jugador;
+import com.qatar.proyecto.entities.Usuario;
 import com.qatar.proyecto.services.IEquipoService;
 import com.qatar.proyecto.services.IJugadorService;
 
@@ -41,9 +46,34 @@ public class JugadorController {
 		return jugadorService.getAll();
 	}
 	
+	@GetMapping("/{id}")
+	public Jugador buscarJugadorPorId(@PathVariable Long id) {
+		return jugadorService.buscar(id);
+	}
+	
 	@PostMapping
 	public void agregarJugador(@RequestBody Jugador jugador) {
 		jugadorService.save(jugador);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Jugador> eliminarJugador(@PathVariable Long id) {
+		
+		Jugador jugador = jugadorService.buscar(id);
+		
+		if(jugador != null) {
+			
+			int rows = jugadorService.eliminarJugador(id);
+			
+			if(rows > 0) {
+				return new ResponseEntity<Jugador>(HttpStatus.CREATED);
+			}else {
+				return new ResponseEntity<Jugador>(HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+		}else {
+			return new ResponseEntity<Jugador>(HttpStatus.EXPECTATION_FAILED);
+		}
+		
 	}
 	
 	}
