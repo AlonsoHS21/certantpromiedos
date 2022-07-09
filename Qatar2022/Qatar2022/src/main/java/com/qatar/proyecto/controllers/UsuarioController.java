9f78a5c3588a7db2183be98446f891b350cd6a7e
Alonso <61801479+AlonsoHS21@users.noreply.github.com>
2022-07-09 16:48:29 -0300

    Controllador para mostrar la lista de usuarios con thymeleaf

--- Qatar2022/Qatar2022/src/main/java/com/qatar/proyecto/controllers/UsuarioController.java
+++ Qatar2022/Qatar2022/src/main/java/com/qatar/proyecto/controllers/UsuarioController.java
@@ -5,32 +5,67 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.mysql.cj.xdevapi.Result;
-import com.qatar.proyecto.entities.Equipo;
-import com.qatar.proyecto.entities.Jugador;
 import com.qatar.proyecto.entities.Usuario;
 import com.qatar.proyecto.services.implementation.UsuarioService;
 
+
+@Controller
+@RequestMapping("/usuario")
+public class UsuarioController {
+	
+	@Autowired
+	@Qualifier("usuarioService") 
+	private UsuarioService usuarioService;
+
+	@GetMapping("/")
+	public String lista(Model model) {
+		List<Usuario> listaUsuarios = usuarioService.listarUsuarios();
+		model.addAttribute("usuarios",listaUsuarios);
+		return "usuario/lista";
+	}
+	
+}
+		/*
+		@PostMapping("/")
+		public String guardar(@Valid @ModelAttribute Usuario usuario,BindingResult result, Model model, RedirectAttributes attribute ) {
+
+			if(result.hasErrors()) {
+				System.out.println("Hubo error en el formulario!");
+				return "usuario/crear";
+			}
+			usuarioService.guardar(usuario);
+			System.out.println("Usuario guardado con exito!");
+			attribute.addFlashAttribute("success","Usuario creado con exito");
+			return "redirect:/usuario/";
+		
+		
+		 */
+	
+	
+/* Links Utiles para las anotaciones
+ *  https://ricardogeek.com/spring-boot-y-la-anotacion-crossorigin/
+ *  https://pharos.sh/controller-y-restcontroller-anotaciones-en-spring-boot/
+ *  https://www.arquitecturajava.com/spring-qualifier-utilizando-autowired/
+ *  https://mvitinnovaciontecnologica.wordpress.com/2020/02/06/guia-de-anotaciones-de-spring-framework/
+ *  https://gustavopeiretti.com/estructura-de-paquetes-spring-boot/
+ *  Nota: @ResponseBody Cuando los controladores de solicitudes devuelven datos, como return repository.findById(), la respuesta se serializará en JSON antes de devolverse al cliente.
+ * */
+	
+	
+
+
+/* ----------------------------- SWAGGR ----------------------------- */
+
+/*
 @RestController
 @RequestMapping("/api")
 public class UsuarioController {
@@ -98,52 +133,6 @@ public class UsuarioController {
 				return new ResponseEntity<Usuario>(HttpStatus.INTERNAL_SERVER_ERROR); //Hubo un error y la solicitud no pude ser completada
 			}
 	}
-}
-	
-	/*
-	@PostMapping("/")
-	public String guardar(@Valid @ModelAttribute Usuario usuario,BindingResult result, Model model, RedirectAttributes attribute ) {
+}*/
 
-		if(result.hasErrors()) {
-			System.out.println("Hubo error en el formulario!");
-			return "usuario/crear";
-		}
-		usuarioService.guardar(usuario);
-		System.out.println("Usuario guardado con exito!");
-		attribute.addFlashAttribute("success","Usuario creado con exito");
-		return "redirect:/usuario/";
-	
-	/*
-	
-	//@GetMapping es un atajo para @RequestMapping(method = RequestMethod.GET) y se utiliza para mapear HTTP GET solicitudes a los métodos de controlador asignados. 
-	@GetMapping 
-	public List<Usuario> listar() {
-		return usuarioService.listarUsuarios();
-	}
-	
-	@PostMapping("/save")
-	public String guardar(@Valid @ModelAttribute Usuario usuario,BindingResult result, Model model, RedirectAttributes attribute ) {
-
-		if(result.hasErrors()) {
-			System.out.println("Hubo error en el formulario!");
-			return "usuario/crear";
-		}
-		usuarioService.guardar(usuario);
-		System.out.println("Usuario guardado con exito!");
-		attribute.addFlashAttribute("success","Usuario creado con exito");
-		return "redirect:/usuario/";
-		*/
-	
-	
-	
-	
-/* Links Utiles para las anotaciones
- *  https://ricardogeek.com/spring-boot-y-la-anotacion-crossorigin/
- *  https://pharos.sh/controller-y-restcontroller-anotaciones-en-spring-boot/
- *  https://www.arquitecturajava.com/spring-qualifier-utilizando-autowired/
- *  https://mvitinnovaciontecnologica.wordpress.com/2020/02/06/guia-de-anotaciones-de-spring-framework/
- *  https://gustavopeiretti.com/estructura-de-paquetes-spring-boot/
- *  Nota: @ResponseBody Cuando los controladores de solicitudes devuelven datos, como return repository.findById(), la respuesta se serializará en JSON antes de devolverse al cliente.
- * */
-	
 
