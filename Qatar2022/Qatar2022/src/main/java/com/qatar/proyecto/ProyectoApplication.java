06f01e03c22996dc3b6c5cd5ac4e749734acd43b
Alonso <61801479+AlonsoHS21@users.noreply.github.com>
2022-07-09 17:56:01 -0300

    Creacion de la vista de lista de quipos y controller

--- Qatar2022/Qatar2022/src/main/java/com/qatar/proyecto/ProyectoApplication.java
+++ Qatar2022/Qatar2022/src/main/java/com/qatar/proyecto/ProyectoApplication.java
@@ -3,6 +3,9 @@ package com.qatar.proyecto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.qatar.proyecto.entities.Equipo;
+import com.qatar.proyecto.entities.Jugador;
+
 @SpringBootApplication
 public class ProyectoApplication {
 
