a9f2c36a9d4d246a5c56dbfb6c029005cd4fa289
Alonso <61801479+AlonsoHS21@users.noreply.github.com>
2022-07-09 17:56:58 -0300

    Creacion de la vista de lista de jugadores y su controller

--- Qatar2022/Qatar2022/src/main/java/com/qatar/proyecto/controllers/JugadorController.java
+++ Qatar2022/Qatar2022/src/main/java/com/qatar/proyecto/controllers/JugadorController.java
@@ -15,8 +15,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -27,10 +29,8 @@ import com.qatar.proyecto.services.IEquipoService;
 import com.qatar.proyecto.services.IJugadorService;
 
 
-//@Controller
-//@RequestMapping("/jugador")
-@RestController
-@RequestMapping("/api/jugador")
+@Controller
+@RequestMapping("/jugador")
 public class JugadorController {
 	
 	@Autowired
@@ -41,51 +41,14 @@ public class JugadorController {
 	@Qualifier("equipoService")
 	private IEquipoService equipoService;
 	
-	@GetMapping
-	public List<Jugador> listaJugadores(){
-		return jugadorService.getAll();
-	}
-	
-	@GetMapping("/{id}")
-	public Jugador buscarJugadorPorId(@PathVariable Long id) {
-		return jugadorService.buscar(id);
-	}
-	
-	@PostMapping
-	public void agregarJugador(@RequestBody Jugador jugador) {
-		jugadorService.save(jugador);
-	}
-	
-	@DeleteMapping("/{id}")
-	public ResponseEntity<Jugador> eliminarJugador(@PathVariable Long id) {
-		
-		Jugador jugador = jugadorService.buscar(id);
-		
-		if(jugador != null) {
-			
-			int rows = jugadorService.eliminarJugador(id);
-			
-			if(rows > 0) {
-				return new ResponseEntity<Jugador>(HttpStatus.CREATED);
-			}else {
-				return new ResponseEntity<Jugador>(HttpStatus.INTERNAL_SERVER_ERROR);
-			}
-		}else {
-			return new ResponseEntity<Jugador>(HttpStatus.EXPECTATION_FAILED);
-		}
-		
-	}
-	
-	}
-/*
- * 
- * @GetMapping("/")
+ 	@GetMapping("/")
 	public String lista(Jugador jugador, Model model) {
 		List<Jugador> listaJugadores = jugadorService.getAll();
-		model.addAttribute("jugador", listaJugadores);
+		model.addAttribute("jugadores", listaJugadores);
 		return "jugador/lista";
 	}
-
+}
+	/*
 	@RequestMapping("/crear")
 	public String crear(Model model) {
 		Jugador jugador = new Jugador();
@@ -110,4 +73,75 @@ public class JugadorController {
 		attribute.addFlashAttribute("success","Jugador creado con exito");
 		return "redirect:/jugador/";
  */
+
+/* -------------------------- SWAGGR ---------------------- */
+/*
+//@RestController
+//@RequestMapping("/api/jugador")
+public class JugadorController {
+	
+	@Autowired
+	@Qualifier("jugadorService")
+	private IJugadorService jugadorService;
+	
+	@Autowired
+	@Qualifier("equipoService")
+	private IEquipoService equipoService;
+	
+	@GetMapping
+	public List<Jugador> listaJugadores(){
+		System.out.println("Entro en la lista de jugadores");
+		return jugadorService.getAll();
+	}
+	
+	@GetMapping("/{id}")
+	public Jugador buscarJugadorPorId(@PathVariable Long id) {
+		return jugadorService.buscar(id);
+	}
+	
+	@PostMapping
+	public void agregarJugador(@RequestBody Jugador jugador) {
+		jugadorService.save(jugador);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Jugador> actualizarJugador(
+			@PathVariable Long id,
+			@RequestBody Jugador jugador
+			){
+		Jugador jugadorEncontrado = jugadorService.buscar(id); //Busco para ver si existe el equipo porque quizas el id recibido no exista
+		
+		if(jugadorEncontrado != null) {
+			
+			int rows = jugadorService.actualizarJugador(jugador.getApellido(), jugador.getNombre(),jugador.getGoles(), jugador.getEquipo().getIdEquipo(),id);
+			
+			if(rows > 0 ) {
+				return new ResponseEntity<Jugador>(HttpStatus.CREATED);
+			}else {
+				return new ResponseEntity<Jugador>(HttpStatus.INTERNAL_SERVER_ERROR); 			}
+		}
+		return new ResponseEntity<Jugador>(HttpStatus.BAD_REQUEST); 
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Jugador> eliminarJugador(@PathVariable Long id) {
+		
+		Jugador jugador = jugadorService.buscar(id);
+		
+		if(jugador != null) {
+			
+			int rows = jugadorService.eliminarJugador(id);
+			
+		if(rows > 0) {
+				return new ResponseEntity<Jugador>(HttpStatus.CREATED);
+			}else {
+				return new ResponseEntity<Jugador>(HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+		}else {
+			return new ResponseEntity<Jugador>(HttpStatus.EXPECTATION_FAILED);
+		}
+		
+	}
+	
+	}*/
 
