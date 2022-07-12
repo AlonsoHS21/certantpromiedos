4c1cbc54f8a254deffcd29243013d6e822747a50
Alonso <61801479+AlonsoHS21@users.noreply.github.com>
2022-07-12 19:38:16 -0300

    Actualizo query para actualizar jugadores agrego el parametro dorsal

--- Qatar2022/Qatar2022/src/main/java/com/qatar/proyecto/repositories/IPartidoRepository.java
+++ Qatar2022/Qatar2022/src/main/java/com/qatar/proyecto/repositories/IPartidoRepository.java
@@ -1,8 +1,8 @@
 package com.qatar.proyecto.repositories;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.qatar.proyecto.entities.Partido;
 
 @Repository("partidoRepository")
