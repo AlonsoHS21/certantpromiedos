d9427082fb4b2eae83bb2c56f1f198a1064c2952
Alonso <61801479+AlonsoHS21@users.noreply.github.com>
2022-07-14 13:18:44 -0300

    Creo actualizar partido y buscar partido por id equipo en partidorepository

--- Qatar2022/Qatar2022/src/main/java/com/qatar/proyecto/entities/Equipo.java
+++ Qatar2022/Qatar2022/src/main/java/com/qatar/proyecto/entities/Equipo.java
@@ -9,9 +9,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "equipo")
@@ -34,7 +37,6 @@ public class Equipo {
 			orphanRemoval = true
 			)
 	private List<Jugador> jugadores = new ArrayList<>();
-	
 	
 	 public Equipo() {
 	    	
--- Qatar2022/Qatar2022/src/main/java/com/qatar/proyecto/repositories/IPartidoRepository.java
+++ Qatar2022/Qatar2022/src/main/java/com/qatar/proyecto/repositories/IPartidoRepository.java
@@ -2,10 +2,20 @@ package com.qatar.proyecto.repositories;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 import com.qatar.proyecto.entities.Partido;
 
 @Repository("partidoRepository")
 public interface IPartidoRepository extends JpaRepository<Partido, Long>{
 	
+	@Query(value="Select * from Partido p Where p.id_partido=(:idPartido)", nativeQuery=true)
+	public abstract Partido buscarPartidoPorId(@Param("idPartido") Long idPartido);
+	
+	@Modifying
+	@Query("Update Partido set estadio=(:estadio), estado_apuesta=(:estadoApuesta),fase=(:fasePartido),fecha=(:fechaPartido), idequipo_local=(:idEquipoLocal), idequipo_visitante=(:idEquipoVisitante) Where id_partido=(:idPartido) ")
+	public abstract int actualizarPartido(@Param("estadio")String estadio,@Param("estadoApuesta") String estadoApuesta,@Param("fasePartido") String fasePartido,@Param("fechaPartido") String fechaPartido,@Param("idEquipoLocal") Long equipoLocal,@Param("idEquipoVisitante") Long equipoVisitante,@Param("idPartido") Long idPartido); 
 }
